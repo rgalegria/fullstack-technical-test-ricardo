ba57afaea70a767efaaf216e487b92f80787b009
Ricardo GOMEZ ALEGRIA <contact.ricardoalegria@gmail.com>
2022-04-23 17:41:35 +0200

    [Add] Search bar component

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,42 +1,38 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch } from "react-instantsearch-dom";
+import { InstantSearch, SearchBox } from "react-instantsearch-dom";
 
-const searchClient = algoliasearch(
-  "latency",
-  "6be0576ff61c053d5f9a3225e2a90f76"
-);
+// Styles
+import styles from "../styles/Home.module.css";
+
+const searchClient = algoliasearch("latency", "6be0576ff61c053d5f9a3225e2a90f76");
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>La Fourche</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to La Fourche Frontend Technical Test v2 !
-        </h1>
-        <InstantSearch indexName="bestbuy" searchClient={searchClient}>
-          {/* ##################################
-              Your instantsearch code goes here.
-              ################################## */}
-        </InstantSearch>
-      </main>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to La Fourche Frontend Technical Test v2 !</h1>
+                <InstantSearch indexName="bestbuy" searchClient={searchClient}>
+                    <header>
+                        <SearchBox translations={{ placeholder: "Barre de recherche" }} />
+                    </header>
+                </InstantSearch>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  );
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                </a>
+            </footer>
+        </div>
+    );
 }
